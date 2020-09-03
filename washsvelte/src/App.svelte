37b94a35af22ee14c17680fa22948256801a7a42
Daniel Juarez <daajt1@gmail.com>
2020-09-03 15:34:36 -0500

    washingyourhands

---
+++ washsvelte/src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+	import Timer from "./Timer.svelte";
+	import HowTo from "./HowTo.svelte";
+	
+  </script>
+  
+  <style>
+	h1,
+	h3 {
+	  text-align: center;
+	}
+  </style>
+  
+  <h1>Handwashing App</h1>
+  <h3>Adviser for covid</h3>
+  
+  <Timer  />
+  
+  <HowTo />
---
+++ washsvelte/src/HowTo.svelte
@@ -0,0 +1,26 @@
+<script>
+  let cont = 0;
+  function handleClick(cont) {
+    return (cont += 1);
+  }
+</script>
+
+<style>
+  img {
+    width: 80%;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+  }
+</style>
+
+<div bp="grid">
+
+  <img
+    bp="offset-5@md 4@md 12@sm"
+    src="corona.jpg"
+    alt="How to wash your hands." />
+  <button on:click={handleClick} />
+
+</div>
